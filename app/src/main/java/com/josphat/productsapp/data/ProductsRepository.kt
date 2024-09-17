079b5345dc87f82fef8d39870f6f78a0f4271934
josphatmwania <josphatmwania237@gmail.com>
2024-09-18 02:18:35 +0300

    feat: implement Products repository to get Products in a list form

--- app/src/main/java/com/josphat/productsapp/data/ProductsRepository.kt
+++ app/src/main/java/com/josphat/productsapp/data/ProductsRepository.kt
@@ -1,4 +1,10 @@
 package com.josphat.productsapp.data
 
+import com.josphat.productsapp.data.model.Products
+import kotlinx.coroutines.flow.Flow
+
 interface ProductsRepository {
+
+//    Todo: get Products in a list form
+    suspend fun getProductList() : Flow<Result<List<Products>>>
 }
