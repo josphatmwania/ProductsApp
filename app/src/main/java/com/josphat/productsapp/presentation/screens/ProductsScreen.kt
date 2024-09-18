c46b860f0ade9fd5dace29d52b78ddd3151dcd09
josphatmwania <josphatmwania237@gmail.com>
2024-09-18 12:54:11 +0300

    feat: create presentation module

---
+++ app/src/main/java/com/josphat/productsapp/presentation/screens/ProductsScreen.kt
@@ -0,0 +1,2 @@
+package com.josphat.productsapp.presentation.screens
+
---
+++ app/src/main/java/com/josphat/productsapp/presentation/viewmodel/ProductsViewModel.kt
@@ -0,0 +1,13 @@
+package com.josphat.productsapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.josphat.productsapp.data.ProductsRepository
+
+class ProductsViewModel(
+    private val productsRepository: ProductsRepository
+) : ViewModel() {
+
+    // immutable stateflow
+
+
+}
