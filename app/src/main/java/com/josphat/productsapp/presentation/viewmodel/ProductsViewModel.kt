360534cc480c5cc5bcceb1135174a7264c439c53
josphatmwania <josphatmwania237@gmail.com>
2024-09-19 22:43:03 +0300

    feat: implement the viewmodel

--- app/src/main/java/com/josphat/productsapp/presentation/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/com/josphat/productsapp/presentation/viewmodel/ProductsViewModel.kt
@@ -2,12 +2,49 @@ package com.josphat.productsapp.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.josphat.productsapp.data.ProductsRepository
+import com.josphat.productsapp.data.model.Product
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 
 class ProductsViewModel(
     private val productsRepository: ProductsRepository
 ) : ViewModel() {
 
-    // immutable stateflow
+    //tODO:  immutable stateflow of List of products
+
+    private val _products = MutableStateFlow<List<Product>>(emptyList())
+
+    val products = _products.asStateFlow()
+
+
+    // tODO: Show a toast on error
+    private val _showErrorToastChannel= Channel<Boolean>()
+    val showErrorToastChannel = _showErrorToastChannel.receiveAsFlow()
+
+
+
+//     todo: get the data in a coroutine
+
+    init {
+        productsRepository.getProductList().collectLatest { result ->
+            when(result) {
+                _showErrorToastChannel.send(true)
+            }
+            is Result.succes -> {
+                result.data?.let { products ->
+                    _products.update { products }
+                }
+        }
+        }
+
+    }
+
+
+
 
 
 }
