6192c9e1e2b32a0f9690e04cf99a80fff0527b21
josphatmwania <josphatmwania237@gmail.com>
2024-09-19 23:13:22 +0300

    fix: add android permission

--- app/src/main/java/com/josphat/productsapp/MainActivity.kt
+++ app/src/main/java/com/josphat/productsapp/MainActivity.kt
@@ -12,6 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.josphat.productsapp.ui.theme.ProductsAppTheme
 
+
+//Todo: Viewmodel factory & pass Products repository implementation
+
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
