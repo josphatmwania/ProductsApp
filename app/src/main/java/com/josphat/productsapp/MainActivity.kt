a92620f4fa31bc0a7bf36241268096ccbfa96f28
josphatmwania <josphatmwania237@gmail.com>
2024-09-19 23:34:11 +0300

    feat: add Products screen

--- app/src/main/java/com/josphat/productsapp/MainActivity.kt
+++ app/src/main/java/com/josphat/productsapp/MainActivity.kt
@@ -10,12 +10,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.josphat.productsapp.data.ProductsRepositoryImpl
+import com.josphat.productsapp.data.remote.RetrofitInstance
+import com.josphat.productsapp.presentation.screens.ProductsScreen
+import com.josphat.productsapp.presentation.viewmodel.ProductsViewModel
 import com.josphat.productsapp.ui.theme.ProductsAppTheme
 
 
 //Todo: Viewmodel factory & pass Products repository implementation
-
-
+private val viewModel by viewModels<ProductsViewModel>(factoryProducer = {
+    object : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return ProductsViewModel(ProductsRepositoryImpl(RetrofitInstance.productAPI))
+                    as T
+        }
+    }
+})
 
 
 class MainActivity : ComponentActivity() {
@@ -28,25 +40,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ProductsScreen(viewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProductsAppTheme {
-        Greeting("Android")
-    }
-}
