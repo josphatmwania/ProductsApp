bed5de23783c1b2a524141751f1d07a8af8f5a2e
josphatmwania <josphatmwania237@gmail.com>
2024-10-15 12:40:39 +0300

    feat: add ProductEntity data class

---
+++ app/src/main/java/com/josphat/productsapp/data/db/ProductEntity.kt
@@ -0,0 +1,23 @@
+package com.josphat.productsapp.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "products")
+data class ProductEntity(
+    @PrimaryKey
+    val id: Int,
+    val brand: String,
+    val description: String,
+    val discountPercentage: Double,
+    val id: Int,
+    val images: List<String>,
+    val price: Double,
+    val rating: Double,
+    val stock: Int,
+    val thumbnail: String,
+    val title: String
+
+)
+
