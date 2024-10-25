1987f29c5c5f4ae610af9c7d80add15b9774f09e
josphatmwania <josphatmwania237@gmail.com>
2024-10-25 19:53:51 +0300

    Add Logs

--- app/src/main/java/com/josphat/productsapp/presentation/screens/ProductDetailsScreen.kt
+++ app/src/main/java/com/josphat/productsapp/presentation/screens/ProductDetailsScreen.kt
@@ -1,10 +1,12 @@
 package com.josphat.productsapp.presentation.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +20,14 @@ import com.josphat.productsapp.presentation.viewmodel.ProductsViewModel
 fun ProductDetailsScreen(productId: Int, viewModel: ProductsViewModel) {
     // Collect the product by its ID using the correct ViewModel function
     val product = viewModel.getProductByID(productId).collectAsState(initial = null).value
+
+    LaunchedEffect(productId) {
+        product?.let {
+            Log.d("PRODUCT_ID", it.description)
+        }
+//        Log.d("PRODUCT_ID", product.description ?: "Product is null")
+//        Log.d("PRODUCT_ID", product.title ?: "Title is null")
+    }
 
     product?.let { productItem ->
         Column(
